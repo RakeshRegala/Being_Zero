5ef733df15b27ed212cbd10bdfa0d07bc9dbef69
RakeshRegala <150713451+RakeshRegala@users.noreply.github.com>
2025-11-09 14:19:29 +0530

    Add Java program to sum three integers

---
+++ Summing Triplets/Code.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner  sc=new Scanner(System.in);
+        long a=sc.nextInt();
+        long b=sc.nextInt();
+        long c=sc.nextInt();
+        System.out.println(a+b+c);
+    }
+}
